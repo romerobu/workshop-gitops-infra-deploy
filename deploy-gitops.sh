1905c35de499ee26a6f167959c815dc6f1fcfbed
Coral <40232754+romerobu@users.noreply.github.com>
2023-03-08 18:10:28 +0100

    Update deploy-gitops.sh

--- deploy-gitops.sh
+++ deploy-gitops.sh
@@ -81,7 +81,7 @@ for i in $(seq 1 $CLUSTER);do
    pod=$(oc get pods -o custom-columns=POD:.metadata.name --no-headers -n keycloak --field-selector=status.phase=Running)
  
    oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh create realms -s realm=myrealm-$i -s enabled=true --no-config --server http://localhost:8080 --realm master --user admin --password admin"
-   oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh create clients -r myrealm -s clientId=myclient-$i -s enabled=true --no-config --server http://localhost:8080 --realm master --user admin --password admin" 
+   oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh create clients -r myrealm-$i -s clientId=myclient-$i -s enabled=true --no-config --server http://localhost:8080 --realm master --user admin --password admin" 
    id=$(oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh get clients -q clientId=myclient-$i -r myrealm-$i --fields id --format csv --noquotes --no-config --server http://localhost:8080 --realm master --user admin --password admin" | sed -n '2p')
    oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh update clients/${id:0:$(expr length $id)-1} -s 'redirectUris=[\"https://oauth-openshift.apps.sno-$i.sandbox2471.opentlc.com/oauth2callback/keycloak/*\"]' -s 'directAccessGrantsEnabled=true' -r myrealm --no-config --server http://localhost:8080 --realm master --user admin --password admin"
    oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh create users -s username=myuser-$i -s enabled=true -r myrealm-$i --no-config --server http://localhost:8080 --realm master --user admin --password admin"
