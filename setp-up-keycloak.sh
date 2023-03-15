2deb4fd26299d73840a939370bd68952a6ae27ac
romerobu <coralrb9722@gmail.com>
2023-03-15 18:51:28 +0100

    resolved conflicts

--- setp-up-keycloak.sh
+++ setp-up-keycloak.sh
@@ -24,7 +24,6 @@ echo ""
 
 for i in $(seq 1 $CLUSTER);do
 
-   export KUBECONFIG=./install/install-dir-sno-$i/auth/kubeconfig
    # Configure keycloak realm, client and users
    pod=$(oc get pods -o custom-columns=POD:.metadata.name --no-headers -n keycloak --field-selector=status.phase=Running)
  
@@ -32,10 +31,10 @@ for i in $(seq 1 $CLUSTER);do
    oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh create clients -r myrealm-$i -s clientId=myclient-$i -s enabled=true --no-config --server http://localhost:8080 --realm master --user admin --password admin" 
    id=$(oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh get clients -q clientId=myclient-$i -r myrealm-$i --fields id --format csv --noquotes --no-config --server http://localhost:8080 --realm master --user admin --password admin" | sed -n '2p')
 
-   oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh update clients/${id:0:$(expr length $id)-1} -s 'redirectUris=[\"https://oauth-openshift.apps.sno-$i.$domain/oauth2callback/keycloak/*\"]' -s 'directAccessGrantsEnabled=true' -r myrealm-$i --no-config --server http://localhost:8080 --realm master --user admin --password admin"
+   oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh update clients/${id:0:$(expr length $id)-1} -s 'redirectUris=[\"https://oauth-openshift.apps.sno-$i.$DOMAIN/oauth2callback/keycloak/*\"]' -s 'directAccessGrantsEnabled=true' -r myrealm-$i --no-config --server http://localhost:8080 --realm master --user admin --password admin"
    #oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh update clients/${id:0:$(expr length $id)-1} -s 'redirectUris=[\"https://*\"]' -s 'directAccessGrantsEnabled=true' -r myrealm-$i --no-config --server http://localhost:8080 --realm master --user admin --password admin"
    oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh create users -s username=myuser-$i -s enabled=true -r myrealm-$i --no-config --server http://localhost:8080 --realm master --user admin --password admin"
-   oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh set-password --username myuser-$i --new-password myuser-$i -r myrealm-$i --no-config --server http://localhost:8080 --realm master --user admin --password admin"
+   oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh set-password --username myuser-$i --new-password myuser-$i -r myrealm-$i --temporary --no-config --server http://localhost:8080 --realm master --user admin --password admin"
 
 done
 
