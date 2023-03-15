b1e5d1b3db7380baeeb8b4ff4d1e9d40e4e46933
Coral <40232754+romerobu@users.noreply.github.com>
2023-03-15 18:57:37 +0100

    Update deploy-gitops.sh

--- deploy-gitops.sh
+++ deploy-gitops.sh
@@ -6,22 +6,6 @@ export KUBECONFIG=./install/install-dir-argo-hub/auth/kubeconfig
 
 echo "Login to argo hub cluster"
 
-# Deploy keycloak
-
-oc new-project keycloak
-oc process -f https://raw.githubusercontent.com/keycloak/keycloak-quickstarts/latest/openshift-examples/keycloak.yaml \
-    -p KEYCLOAK_ADMIN=admin \
-    -p KEYCLOAK_ADMIN_PASSWORD=admin \
-    -p NAMESPACE=keycloak \
-| oc create -f -
-
-KEYCLOAK_URL=https://$(oc get route keycloak --template='{{ .spec.host }}') &&
-echo "" &&
-echo "Keycloak:                 $KEYCLOAK_URL" &&
-echo "Keycloak Admin Console:   $KEYCLOAK_URL/admin" &&
-echo "Keycloak Account Console: $KEYCLOAK_URL/realms/myrealm/account" &&
-echo ""
-
 # This must be executed on snos
 # oc -n openshift-ingress-operator get secret router-ca -o jsonpath="{ .data.tls\.crt }" | base64 -d -i > ca.crt
 # oc -n openshift-config create cm keycloak-ca --from-file=ca.crt
@@ -52,7 +36,6 @@ echo "Login to argocd servr..."
 
 for i in $(seq 1 $CLUSTER);do
 
-   
    export KUBECONFIG=./install/install-dir-sno-$i/auth/kubeconfig
    CONTEXT=$(kubectl config get-contexts -o name)
    argocd proj create project-sno-$i
@@ -78,16 +61,6 @@ for i in $(seq 1 $CLUSTER);do
    oc adm groups new admin-sno-$i
    oc adm groups add-users admin-sno-$i user-$i 
    
-   # Configure keycloak realm, client and users
-   pod=$(oc get pods -o custom-columns=POD:.metadata.name --no-headers -n keycloak --field-selector=status.phase=Running)
- 
-   oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh create realms -s realm=myrealm-$i -s enabled=true --no-config --server http://localhost:8080 --realm master --user admin --password admin"
-   oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh create clients -r myrealm-$i -s clientId=myclient-$i -s enabled=true --no-config --server http://localhost:8080 --realm master --user admin --password admin" 
-   id=$(oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh get clients -q clientId=myclient-$i -r myrealm-$i --fields id --format csv --noquotes --no-config --server http://localhost:8080 --realm master --user admin --password admin" | sed -n '2p')
-   oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh update clients/${id:0:$(expr length $id)-1} -s 'redirectUris=[\"https://*\"]' -s 'directAccessGrantsEnabled=true' -r myrealm-$i --no-config --server http://localhost:8080 --realm master --user admin --password admin"
-   oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh create users -s username=myuser-$i -s enabled=true -r myrealm-$i --no-config --server http://localhost:8080 --realm master --user admin --password admin"
-   oc exec -it $pod -- sh -c "/opt/keycloak/bin/kcadm.sh set-password --username myuser-$i --new-password myuser-$i -r myrealm-$i --no-config --server http://localhost:8080 --realm master --user admin --password admin"
-
 done
 
 # Update argo rbac
@@ -97,5 +70,4 @@ oc apply -f ./gitops-operator/argo-instance.yaml
 
 argocd cluster list
 echo "Done adding clusters"
-
 
